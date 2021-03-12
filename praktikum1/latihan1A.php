64d7ec52f505dc866638c1f5136f50c56df3cc47
FaishalThaariq12 <isalthoriq12@gmail.com>
2021-03-12 16:16:02 +0700

    Salinan repository dari prakpw

---
+++ praktikum1/latihan1A.php
@@ -0,0 +1,12 @@
+<!-- 
+    Nama :M. Faishal Thariqulhaq
+    NRP : 20304003
+    Shift : Rabu pukul 09:00-10:00 WIB
+-->
+
+<?php for ($x=1; $x<=3; $x++){
+    for ($z=1; $z<=3; $z++){
+        echo "Ini perulangan ke ($x,$z)<br>";
+    }
+}
+?>
