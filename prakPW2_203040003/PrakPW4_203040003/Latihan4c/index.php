c02df521de39a2f2dad0c16db40b42e4fec321c6
FaishalThaariq12 <isalthoriq12@gmail.com>
2021-04-15 10:22:14 +0700

    update eror pada detail

--- prakPW2_203040003/PrakPW4_203040003/Latihan4c/index.php
+++ prakPW2_203040003/PrakPW4_203040003/Latihan4c/index.php
@@ -7,21 +7,18 @@
 ?>
 
   
-<?php
-
-// Menghubungkan dengan file php lainya 
+<?php 
 require 'php/functions.php';
-
-//melakukan query biasa ke table hijab
-$bundle = query("SELECT * FROM novel");
+$novel = query("SELECT * FROM novel");
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Buku kehidupan</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="../pw2021_203040008/latihan4b/css/style.css">
     <style>
         .table {
             width: 100%;
@@ -32,40 +29,24 @@ $bundle = query("SELECT * FROM novel");
             max-width: 150px;
         }
         </style>
-    </head>
-<body>
-<script type="text/javascript" src="js/materialize.min.js"></script>
-<div class="container">
-    <table cellpadding="10" cellspacing="0" border="1" class="highlight - centered brown lighten-4">
-        <tr>
-                <th scope="col">No</th>
-                <th scope="col">Judul</th>
-                <th scope="col">Gambar</th>
-                <th scope="col">Pengarang</th>
-                <th scope="col">Sinopsis</th>
-                <th scope="col">Harga</th>
-                <th scope="col">Kategori</th>
-            </tr>
-        </thead>
-        <tbody>
-        <?php $i = 1;
-            foreach ($novel as $novel) :
-            ?>
-                <tr>
-            <td><?= $i; ?></td>
-            <td><?= $row["judul"]; ?></td>
-            <td><img src="assets/img/<?= $row["gambar"];?> "></td>
-            <td><?= $row["pengarang"]; ?></td>
-            <td><?= $row["sinopsis"]; ?></td>
-            <td><?= $row["harga"]; ?></td>
-            <td><?= $row["kategori"];?> </td>
-        </tr>
-        <?php $i++ ?>
-        <?php endforeach; ?>
-        </tbody>
-    </table>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
-    </script>
-</body>
 
+    <title>Buku kehidupan</title>
+</head>
+<body>
+    
+    <div class="container">
+        <div class="table-primary"></div>
+            <?php foreach ($novel as $nv) :?>
+                <p class="nama">
+                    <a href="php/detail.php?id=<?= $nv['id'] ?>">
+                        <?= $nv["pengarang"]?>
+                    </a>
+                </p>
+            <?php endforeach; ?>
+        </div>
+    </div>
+
+        </table>
+    </div>
+</body>
 </html>
